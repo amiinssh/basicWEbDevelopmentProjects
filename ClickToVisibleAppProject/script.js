98c869b8d37588579fb3f57b8557e7a2746fe2eb
amiinssh <145289316+amiinssh@users.noreply.github.com>
2024-09-09 04:30:18 -1200

    Add files via upload

---
+++ ClickToVisibleAppProject/script.js
@@ -0,0 +1,79 @@
+const m = document.getElementById("m");
+const o = document.getElementById("o");
+const h = document.getElementById("h");
+const a = document.getElementById("a");
+const m1 = document.getElementById("m1");
+const m2 = document.getElementById("m2");
+const a1 = document.getElementById("a1");
+const d = document.getElementById("d");
+const a2 = document.getElementById("a2");
+const m3 = document.getElementById("m3");
+const i = document.getElementById("i");
+const n = document.getElementById("n");
+const mohammdamin = document.getElementById("complete");
+
+m.addEventListener("click", () => {
+    m.style.display = "none";
+    o.style.display = "block";
+});
+
+o.addEventListener("click", () => {
+    o.style.display = "none";
+    h.style.display = "block";
+});
+
+h.addEventListener("click", () => {
+    h.style.display = "none";
+    a.style.display = "block";
+});
+
+a.addEventListener("click", () => {
+    a.style.display = "none";
+    m1.style.display = "block";
+});
+
+m1.addEventListener("click", () => {
+    m1.style.display = "none";
+    m2.style.display = "block";
+});
+
+m2.addEventListener("click", () => {
+    m2.style.display = "none";
+    a1.style.display = "block";
+});
+
+a1.addEventListener("click", () => {
+    a1.style.display = "none";
+    d.style.display = "block";
+});
+
+d.addEventListener("click", () => {
+    d.style.display = "none";
+    a2.style.display = "block";
+});
+
+a2.addEventListener("click", () => {
+    a2.style.display = "none";
+    m3.style.display = "block";
+});
+
+m3.addEventListener("click", () => {
+    m3.style.display = "none";
+    i.style.display = "block";
+});
+
+i.addEventListener("click", () => {
+    i.style.display = "none";
+    n.style.display = "block";
+});
+
+n.addEventListener("click", () => {
+    n.style.display = "none";
+    
+});
+
+n.addEventListener("click", () => {
+    n.style.display = "none";
+    mohammdamin.style.display = "block"; 
+});
+
