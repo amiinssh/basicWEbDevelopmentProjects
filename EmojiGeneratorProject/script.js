3d90344e6da39413416c42c2f985ff18a9330d89
amiinssh <145289316+amiinssh@users.noreply.github.com>
2024-10-01 12:04:43 -0700

    Add files via upload

---
+++ EmojiGeneratorProject/script.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector("#emoji");
+const emojis = [
+    "😀", "😃", "😄", "😁", "😆", "😅", "😂", "🤣", "😊", "😇", "🙂", "🙃", "😉", "😍", "🥰", "😘", "😗", "😚", "😋", "😛", "😜", "🤪", "😝", "🤑", "🤗", "🤭", "🤔", "🤐"
+];
+
+btn.addEventListener("mouseover", () => {
+    btn.innerHTML = emojis[Math.floor(Math.random() * emojis.length)]
+});
